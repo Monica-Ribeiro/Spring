5d1ae01411d0b4fe097a51ffdb16a2eaecec7ac7
Monica Ribeiro <monicaspribeiro@gmail.com>
2020-11-09 21:30:04 -0300

    atualizando

---
+++ DeGames/DeGames/src/main/java/com/minhaLoja/DeGames/DeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.minhaLoja.DeGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeGamesApplication.class, args);
+	}
+
+}
---
+++ DeGames/DeGames/src/test/java/com/minhaLoja/DeGames/DeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaLoja.DeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
