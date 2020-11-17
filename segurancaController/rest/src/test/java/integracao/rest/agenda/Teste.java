38574817e0550e60572470720a7dc1913d675ff5
Monica Ribeiro <monicaspribeiro@gmail.com>
2020-11-17 14:21:42 -0300

    Security Test Rest

---
+++ segurancaController/rest/src/test/java/integracao/rest/agenda/Teste.java
@@ -0,0 +1,37 @@
+package integracao.rest.agenda;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import integracao.rest.contatos.Contato;
+import integracao.rest.contatos.ContatoRepository;
+
+@RunWith(SpringRunner.class)   //TESTES WEB
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class Teste {
+	
+	//TESTE COM CONSTRUÇÃO DE CENARIOS
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	//DEPOIS MUDAR O ENDPOINT E TESTAR
+	@Test
+	public void deveMostrarTodosContatos() {
+		ResponseEntity<String>resposta =
+		testRestTemplate.exchange("/contatos/", HttpMethod.GET,null,String.class);
+		Assert.assertEquals(HttpStatus.OK, resposta.getStatusCode());
+	}
+
+	
+
+}
